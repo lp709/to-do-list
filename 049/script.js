5995c83a029ae35096cce28258f7fbc97ff12f85
lp709 <pinheiroleandro7009@gmail.com>
2026-05-07 22:25:16 +0100

    lista ordena, feito com html, css e js, o footer eu escrevi com o js, document.getElementById

---
+++ 049/script.js
@@ -0,0 +1,11 @@
+var author0 = "Made" + " " + "By" + " " + "Lp";
+
+
+
+document.getElementById("footer").innerHTML = author0;
+
+
+console.log("Author :" + " " + "Lp");
+
+var author1 = !false;
+console.error(author1);
