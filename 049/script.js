c0d3f9983937939a53ae9a7e1c756dcde3fbf465
lp709 <pinheiroleandro7009@gmail.com>
2026-05-08 18:34:34 +0100

    mudando algumas definicoes do arquivo script.js (console.log & console.error)

--- 049/script.js
+++ 049/script.js
@@ -7,5 +7,7 @@ document.getElementById("footer").innerHTML = author0;
 
 console.log("Author :" + " " + "Lp");
 
-var author1 = !false;
+var author1 = !true;
+var author2 = !false;
 console.error(author1);
+console.log(author2);
